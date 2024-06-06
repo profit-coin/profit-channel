f8aa1d96c4e5d9a4b06513fd77a5a4e9103d215c
Manfies <okunevden97@gmail.com>
2024-06-06 14:40:17 +0200

    chore: init for telegram mini apps

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,9 +1,21 @@
-import { Html, Head, Main, NextScript } from "next/document";
+import { Html, Head, Main, NextScript } from 'next/document';
 
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        {
+          // eslint-disable-next-line @next/next/no-sync-scripts
+          <script src={'https://telegram.org/js/telegram-web-app.js'}></script>
+        }
+        {
+          // eslint-disable-next-line @next/next/no-sync-scripts
+          <script src="https://cdn.jsdelivr.net/npm/eruda"></script>
+        }
+        <script>
+          eruda.init();
+        </script>
+      </Head>
       <body>
         <Main />
         <NextScript />
