84b4e6c179970b3c5feb165a2768a3f2da50cadc
Sergey Eroshenkov <itechmeat@gmail.com>
2024-06-07 23:10:46 +0200

    Tune project's configs

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,4 +1,4 @@
-import { Html, Head, Main, NextScript } from 'next/document';
+import { Head, Html, Main, NextScript } from 'next/document'
 
 export default function Document() {
   return (
@@ -12,14 +12,12 @@ export default function Document() {
           // eslint-disable-next-line @next/next/no-sync-scripts
           <script src="https://cdn.jsdelivr.net/npm/eruda"></script>
         }
-        <script>
-          eruda.init();
-        </script>
+        <script>eruda.init();</script>
       </Head>
       <body>
         <Main />
         <NextScript />
       </body>
     </Html>
-  );
+  )
 }
