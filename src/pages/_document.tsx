e532c1dd7599bf1d48353fbfde42a9e4f1567c29
Sergey Eroshenkov <itechmeat@gmail.com>
2024-06-08 11:49:42 +0200

    Add font

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -4,6 +4,12 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="" />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Bungee+Spice&display=swap"
+          rel="stylesheet"
+        />
         {
           // eslint-disable-next-line @next/next/no-sync-scripts
           <script src={'https://telegram.org/js/telegram-web-app.js'}></script>
